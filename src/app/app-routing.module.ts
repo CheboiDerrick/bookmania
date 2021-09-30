e3d3dfdb9e985630616bcac990f9f4ed35baaf9e
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-09-30 21:55:52 +0300

    create routing links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BooksviewComponent } from './booksview/booksview.component';
+import { ClassicsComponent } from './classics/classics.component';
+import { FantasyComponent } from './fantasy/fantasy.component';
+import { HistoryComponent } from './history/history.component';
+import { MysteryComponent } from './mystery/mystery.component';
+import { ScienceFictionComponent } from './science-fiction/science-fiction.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'classics', component: ClassicsComponent },
+  { path: 'fantasy', component: FantasyComponent },
+  { path: 'history', component: HistoryComponent },
+  { path: 'romance', component: BooksviewComponent },
+  { path: 'mystery', component: MysteryComponent },
+  { path: 'scify', component: ScienceFictionComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
