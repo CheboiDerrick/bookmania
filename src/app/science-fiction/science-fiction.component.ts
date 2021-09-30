6bb337486fd4d287e1cf04d31ac3d9e9f82514bf
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-09-30 21:55:24 +0300

    science-fiction component

---
+++ src/app/science-fiction/science-fiction.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-science-fiction',
+  templateUrl: './science-fiction.component.html',
+  styleUrls: ['./science-fiction.component.css']
+})
+export class ScienceFictionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
