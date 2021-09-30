637f207019d331b9fbc9b70e2378c4f03b16afa3
CheboiDerrick <derrick.cheboi@student.moringaschool.com>
2021-09-30 13:21:54 +0300

    addedd the book view component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
+import { BooksviewComponent } from './booksview/booksview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     SigninComponent,
-    SignupComponent
+    SignupComponent,
+    BooksviewComponent
   ],
   imports: [
     BrowserModule,
