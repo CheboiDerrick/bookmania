c04b57fd925dcf3a101b8365b4de946383965609
Moses <moses.junior@student.moringaschool.com>
2021-10-01 12:43:26 +0300

    change in structuring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SignupComponent } from './signup/signup.component';
 import { BooksviewComponent } from './booksview/booksview.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
@@ -30,6 +31,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     FormsModule,
     ReactiveFormsModule
   ],
